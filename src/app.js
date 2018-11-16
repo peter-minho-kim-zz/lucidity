f5d2727477357d9ad65cbb028109a6e5e67a1aa3
Peter Kim <peter-minho-kim@gmail.com>
2018-11-16 09:29:56 -0500

    Remove dummy dream data

--- src/app.js
+++ src/app.js
@@ -12,18 +12,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
-store.dispatch(addDream({ title: 'Iron Man', entry: 'this is the dream entry', dreamType: 'lucid', hoursSlept: 6.25, createdAt: 1000 }))
-store.dispatch(addDream({ title: 'Thor', entry: 'this is the dream entry', dreamType: 'normal', hoursSlept: 9.5, createdAt: 400 }))
-store.dispatch(addDream({ title: 'Thanos', entry: 'this is the dream entry', dreamType: 'nightmare', hoursSlept: 8.75, createdAt: 2000 }))
-store.dispatch(addDream({ title: 'Captain America', entry: 'this is the dream entry', dreamType: 'lucid', hoursSlept: 7, createdAt: -60 }))
-
-const state = store.getState()
-const visibleDreams = getVisibleDreams(state.dreams, state.filters)
-
-console.log(visibleDreams)
-
-console.log(store.getState())
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
