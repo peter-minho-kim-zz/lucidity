98cb4ccde27c1c7c890d986563e2b3c5c7f8b693
Peter Kim <peter-minho-kim@gmail.com>
2018-11-17 09:37:58 -0500

    Add and calculate more stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -5,24 +5,47 @@ class Stats extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      rank: ''
+      rank: '',
+      lucidDreamPct: '',
+      totalEntries: this.props.dreams.length,
+      avgSleepTime: 0,
+      avgLucidityLevel: 0
     }
   }
   componentWillMount() {
+    const dreams = this.props.dreams
     let lucidDreamCount = 0
+    let totalSleepTime = 0
+    let totalLucidityLevel = 0
 
-    this.props.dreams.forEach((dream) => {
+    dreams.forEach((dream) => {
       if (dream.dreamType === 'lucid') {
         lucidDreamCount += 1
       }
-    })
 
+      // Calculate average sleep time
+      totalSleepTime += dream.hoursSlept
+      this.state.avgSleepTime = (totalSleepTime / dreams.length).toFixed(1)
+
+      // Calculate average lucidity level
+      totalLucidityLevel += dream.lucidityLevel
+      this.state.avgLucidityLevel = (totalLucidityLevel / dreams.length).toFixed(1)
+    })
+    
+    // Calculate rank
     lucidDreamCount >= 5 ? this.state.rank = 'Dreamwalker' : this.state.rank = 'Stargazer'
+    
+    // Calculate lucid dream percentage
+    this.state.lucidDreamPct = (lucidDreamCount / dreams.length).toFixed(2).toString() + '%' 
   }
   render() {
     return (
       <div>
-        MY RANK: {this.state.rank}
+        <p>RANK: {this.state.rank}</p>
+        <p>LUCID DREAM PERCENTAGE: {this.state.lucidDreamPct}</p>
+        <p>TOTAL ENTRIES: {this.state.totalEntries}</p>
+        <p>AVERAGE SLEEP HOURS: {this.state.avgSleepTime}</p>
+        <p>AVERAGE LUCIDITY LEVEL: {this.state.avgLucidityLevel}</p>
       </div>
     )
   }
