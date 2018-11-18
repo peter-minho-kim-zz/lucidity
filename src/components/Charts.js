0bb0ae1d56dfee40e7a606e8cf22056e5e940eb9
Peter Kim <peter-minho-kim@gmail.com>
2018-11-18 11:59:48 -0500

    Add initial line chart

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,39 +1,107 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Bar, Line, Doughnut} from 'react-chartjs-2'
+import { Line, Doughnut } from 'react-chartjs-2'
+import moment from 'moment'
 
 class Charts extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      chartData: {
-        labels: ['Monday', 'Tuesday', 'Wednesday'],
+      sleepTimeChartData: {
+        labels: this.props.dreams.map((dream) => moment(dream.createdAt).format('MMM Do')).reverse(),
         datasets: [
           {
-            label: 'Days',
-            data: [1, 2, 3],
-            backgroundColor: ['rgb(0, 121, 211)']
+            label: 'Hours Slept',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75, 192, 192, 0.4)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderCapStyles: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75, 192, 192, 1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75, 192, 192, 1)',
+            pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: this.props.dreams.map((dream) => dream.hoursSlept).reverse()
           }
         ]
+      },
+      sleepTimeChartOptions: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              color: 'red'
+            }
+          }],
+          xAxes: [{
+            gridLines: {
+              display: false
+            }
+          }]
+        }
       }
+      // dreamTypeChartData: {
+      //   labels: this.props.dreams.map((dream) => moment(dream.createdAt).format('MMM Do')).reverse(),
+      //   datasets: [
+      //     {
+      //       label: 'Hours Slept',
+      //       fill: false,
+      //       lineTension: 0.1,
+      //       backgroundColor: 'rgba(75, 192, 192, 0.4)',
+      //       borderColor: 'rgba(75, 192, 192, 1)',
+      //       borderCapStyles: 'butt',
+      //       borderDash: [],
+      //       borderDashOffset: 0.0,
+      //       borderJoinStyle: 'miter',
+      //       pointBorderColor: 'rgba(75, 192, 192, 1)',
+      //       pointBackgroundColor: '#fff',
+      //       pointBorderWidth: 1,
+      //       pointHoverRadius: 5,
+      //       pointHoverBackgroundColor: 'rgba(75, 192, 192, 1)',
+      //       pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
+      //       pointHoverBorderWidth: 2,
+      //       pointRadius: 1,
+      //       pointHitRadius: 10,
+      //       data: this.props.dreams.map((dream) => dream.hoursSlept).reverse()
+      //     }
+      //   ]
+      // },
+      // dreamTypeChartOptions: {
+      //   scales: {
+      //     yAxes: [{
+      //       ticks: {
+      //         beginAtZero: true
+      //       },
+      //       gridLines: {
+      //         color: 'red'
+      //       }
+      //     }],
+      //     xAxes: [{
+      //       gridLines: {
+      //         display: false
+      //       }
+      //     }]
+      //   }
+      // }
     }
   }
   render() {
-    // console.log(this.props.dreams)
+    console.log(this.props.dreams)
     return (
       <div>
-        CHART COMPONENT
-        <Bar
-          data={this.state.chartData}
-          options={{
-            title: {
-              display: true,
-              text: 'TITLE TEXT'
-            },
-            legend: {
-              display: true
-            }
-          }}
+        <Line
+          data={this.state.sleepTimeChartData}
+          options={this.state.sleepTimeChartOptions}
         />
       </div>
     )
