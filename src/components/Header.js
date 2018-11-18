e10dbb5fb1fabf7f1b416356c21d890e353bfa3c
Peter Kim <peter-minho-kim@gmail.com>
2018-11-18 14:03:26 -0500

    Conditionally render header based on window location

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
-const Header = () => (
-  <header>
-    <h1>Lucidity</h1>
-    <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/dreams" activeClassName="is-active">Dreams</NavLink>
-    <NavLink to="/create" activeClassName="is-active">New Dream</NavLink>
-  </header>
-)
+const Header = (props) => {
+  return (
+    <header>
+      {window.location.pathname === '/' ? 
+        <Link to="/dashboard">DEMO</Link>
+        : 
+        <div>
+          <h1><Link to='/'>Lucidity</Link></h1>
+          <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
+          <NavLink to="/dreams" activeClassName="is-active">Dreams</NavLink>
+          <NavLink to="/create" activeClassName="is-active">New Dream</NavLink>
+        </div>
+      }
+    </header>
+  )
+}
 
 export default Header
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const HomePage = () => (
-  <div>
-    <button>Demo</button>
-  </div>
-)
-
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>THIS IS THE HOME PAGE</h1>
+    </div>
+  )
+}
