9a7e5fc8a093e5b39b96997f44e5a79cee956bc3
Peter Kim <peter-minho-kim@gmail.com>
2018-11-25 13:00:49 -0500

    Styled dream list item

--- src/components/DreamListItem.js
+++ src/components/DreamListItem.js
@@ -15,15 +15,45 @@ class DreamListItem extends React.Component {
   }
   render() {
     return (
-      <div className="dream">
-        <div className="dream__header" onClick={this.openDream}>
-          <span className="dream__title">{this.props.title}</span>
-          <span className="dream__type">{this.props.dreamType}</span>
-          <span className="dream__hours">{this.props.hoursSlept}</span>
-          <span className="dream__date">{moment(this.props.createdAt).format('MMM Do, YYYY')}</span>
+      <div className="dream" onClick={this.openDream}>
+        <div className="dream__header">
+          <div className="dream__title-box">
+            <span className="dream__title">{this.props.title}</span>
+          </div>
+          <div className="dream__info-container">
+            <div className="dream__info-box">
+              <span className="dream__type">
+                {(() => {
+                  const dreamTypeSplit = this.props.dreamType.split('')
+                  const dreamTypeEdit = dreamTypeSplit.map((letter, index) => {
+                    if (index === 0) {
+                      return letter.toUpperCase()
+                    } else {
+                      return letter
+                    }
+                  })
+                  return dreamTypeEdit.join('')
+                })()}
+              </span>
+            </div>
+            <div className="dream__info-box">
+              <span className="dream__lucidityLevel"><i className="fas fa-signal dream__level"></i>{this.props.lucidityLevel}</span>
+            </div>
+            <div className="dream__info-box">
+              <span className="dream__hours"><i className="far fa-clock dream__clock"></i>{this.props.hoursSlept}</span>
+            </div>
+            <div className="dream__info-box">
+              <span className="dream__date"><i className="far fa-calendar-alt dream__calendar"></i>{moment(this.props.createdAt).format('MMM Do, YYYY')}</span>
+            </div>
+          </div>
         </div>
         <div className="dream__body" ref="dreamBody">
-          <p className="dream__entry"><i class="fas fa-quote-left dream__quote"></i>{this.props.entry}</p>
+          <p className="dream__entry"><i className="fas fa-quote-left dream__quote"></i>{this.props.entry}</p>
+          <div className="dream__options">
+            <Link to={`/edit/${this.props.id}`} className="dream__link">
+              <i className="fas fa-pencil-alt dream__edit"></i>
+            </Link>
+          </div>
         </div>
 
             {/*<Link to={`/edit/${id}`} className="dream__link">
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,42 +1,42 @@
-import * as firebase from 'firebase'
+// import * as firebase from 'firebase'
 
-const config = {
-  apiKey: "AIzaSyBCtULE5lkKUPkiMjJtROebrVPxDDD6c0c",
-  authDomain: "lucidity-app-db.firebaseapp.com",
-  databaseURL: "https://lucidity-app-db.firebaseio.com",
-  projectId: "lucidity-app-db",
-  storageBucket: "",
-  messagingSenderId: "731561970120"
-}
+// const config = {
+//   apiKey: "AIzaSyBCtULE5lkKUPkiMjJtROebrVPxDDD6c0c",
+//   authDomain: "lucidity-app-db.firebaseapp.com",
+//   databaseURL: "https://lucidity-app-db.firebaseio.com",
+//   projectId: "lucidity-app-db",
+//   storageBucket: "",
+//   messagingSenderId: "731561970120"
+// }
 
-firebase.initializeApp(config)
+// firebase.initializeApp(config)
 
-const database = firebase.database()
+// const database = firebase.database()
 
-database.ref().set({
-  name: 'Peter Kim',
-  age: 29,
-  stressLevel: 8,
-  job: {
-    title: 'Software Developer',
-    company: 'Google'
-  },
-  isSingle: true,
-  location: {
-    city: 'Atlanta',
-    state: 'Georgia'
-  }
-}).then(() => {
-  console.log('data is saved')
-}).catch((e) => {
-  console.log('this failed', e)
-})
+// database.ref().set({
+//   name: 'Peter Kim',
+//   age: 29,
+//   stressLevel: 8,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
+//   isSingle: true,
+//   location: {
+//     city: 'Atlanta',
+//     state: 'Georgia'
+//   }
+// }).then(() => {
+//   console.log('data is saved')
+// }).catch((e) => {
+//   console.log('this failed', e)
+// })
 
-database.ref().update({
-  stressLevel: 10,
-  'job/company': 'Amazon',
-  'location/city': 'Seattle'
-})
+// database.ref().update({
+//   stressLevel: 10,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// })
 
 // database.ref('isSingle')
 //   .remove()
