82245124cbad4225f10bf96a9d3a63628af39475
Peter Kim <peter-minho-kim@gmail.com>
2018-11-24 23:00:36 -0500

    Working accordian

--- src/components/DreamListItem.js
+++ src/components/DreamListItem.js
@@ -3,16 +3,51 @@ import { removeDream } from '../actions/dreams'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 
-const DreamListItem = ({ id, title, entry, dreamType, createdAt, hoursSlept }) => (
-  <div>
-    <Link to={`/edit/${id}`}>
-      <h3>title: {title}</h3>
-    </Link>
-    <p>entry: {entry}</p>
-    <p>dreamType: {dreamType}</p>
-    <p>createdAt: {moment(createdAt).format('MMMM Do, YYYY')}</p>
-    <p>hours slept: {hoursSlept}</p>
-  </div>
-)
+class DreamListItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.openDream = this.openDream.bind(this)
+  }
+  openDream() {
+    const dreamBody = this.refs.dreamBody
+    dreamBody.style.maxHeight ? 
+      dreamBody.style.maxHeight = null : dreamBody.style.maxHeight = dreamBody.scrollHeight + 'px'
+  }
+  render() {
+    return (
+      <div className="dream">
+        <div className="dream__header" onClick={this.openDream}>
+          <span className="dream__title">{this.props.title}</span>
+          <span className="dream__type">{this.props.dreamType}</span>
+          <span className="dream__hours">{this.props.hoursSlept}</span>
+          <span className="dream__date">{moment(this.props.createdAt).format('MMM Do, YYYY')}</span>
+        </div>
+        <div className="dream__body" ref="dreamBody">
+          <p className="dream__entry">{this.props.entry}</p>
+        </div>
+
+            {/*<Link to={`/edit/${id}`} className="dream__link">
+              </Link>*/}
+      </div>
+    )
+  }
+}
+
+// const DreamListItem = ({ id, title, entry, dreamType, createdAt, hoursSlept }) => (
+//   <div className="dream">
+//     <div className="dream__header">
+//       <span className="dream__title">{title}</span>
+//       <span className="dream__type">{dreamType}</span>
+//       <span className="dream__hours">{hoursSlept}</span>
+//       <span className="dream__date">{moment(createdAt).format('MMM Do, YYYY')}</span>
+//     </div>
+//     <div className="dream__body">
+//       <p className="dream__entry">{entry}</p>
+//     </div>
+
+//     {/*<Link to={`/edit/${id}`} className="dream__link">
+// </Link>*/}
+//   </div>
+// )
 
 export default DreamListItem
