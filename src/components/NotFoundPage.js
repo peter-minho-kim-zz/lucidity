9177e66f2c7486c6a16a0f8f154e95343864fb66
Peter Kim <peter-minho-kim@gmail.com>
2018-11-30 22:29:16 -0500

    Changes not found page to class-based component

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,14 +1,5 @@
 import React from 'react'
 
-// const NotFoundPage = () => (
-//   <div className="wrapper">
-//     <div className="four-oh-four-page">
-//       <h2 className="four-oh-four">404</h2>
-//       <p className="four-oh-four-p">page not found</p>
-//     </div>
-//   </div>
-// )
-
 class NotFoundPage extends React.Component {
   constructor(props) {
     super(props)
